19a5928db9a179e276e7ef86af262390a5be1d58
Jack Gallant <concise_sparsity287626@outlook.com>
2025-08-24 19:12:30 +0100

    Initial commit

---
+++ hangman.c
@@ -0,0 +1,37 @@
+#include "hangman.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void chooseWord(const char *list[], int listSize, char *word) {
+  srand(time(NULL));
+  int index = rand() % listSize;
+  strncpy(word, list[index], 19);
+  word[19] = '\0';
+}
+
+int isLetterInWord(char letter, const char *word) {
+  for (int i = 0; word[i] != '\0'; i++) {
+    if (word[i] == letter) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void revealLetter(char letter, const char *word, char *display) {
+  for (int i = 0; word[i] != '\0'; i++) {
+    if (word[i] == letter) {
+      display[i] = letter;
+    }
+  }
+}
+
+int isWordGuessed(const char *display) {
+  for (int i = 0; display[i] != '\0'; i++) {
+    if (display[i] == '_') {
+      return 0;
+    }
+  }
+  return 1;
+}
---
+++ main.c
@@ -0,0 +1,47 @@
+#include "hangman.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TRIES 10
+#define WORD_LIST_SIZE 5
+
+const char *wordList[WORD_LIST_SIZE] = {"apple", "banana", "cherry", "date",
+                                        "elderberry"};
+
+int main() {
+    char word[20];
+    char display[20];
+    int tries = MAX_TRIES;
+    char guess;
+
+    chooseWord(wordList, WORD_LIST_SIZE, word);
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        display[i] = '_';
+    }
+    display[strlen(word)] = '\0';
+
+    printf("Welcome to Hangman!\n");
+
+    while (tries > 0) {
+        printf("\n%s\n", display);
+        printf("Guess a letter: ");
+        scanf(" %c", &guess);
+
+        if (isLetterInWord(guess, word)) {
+            revealLetter(guess, word, display);
+            if (isWordGuessed(display)) {
+                printf("\nCongratulations! You guessed the word: %s\n", word);
+                return 0;
+            }
+        }
+        else {
+            tries--;
+            printf("Wrong guess. Tries left: %d\n", tries);
+        }
+    }
+
+    printf("\nGame over! The word was: %s\n", word);
+    return 0;
+}
